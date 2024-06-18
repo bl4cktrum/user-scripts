870d4ef8f1383caa5e97165af706caec758025a0
bl4cktrum <yavuzhancaskurlu@gmail.com>
2024-06-18 18:30:56 +0300

    fine_scroll-yt_music: fix: internally volume changes has been prevented.

--- fine_scroll-yt_music.js
+++ fine_scroll-yt_music.js
@@ -17,8 +17,9 @@
     const SLIDER_SETTERS_SELECTOR = '.slider-knob.style-scope.tp-yt-paper-slider, #sliderBar'
 
     const SLIDE_MULTIPLIER = 2;
+    let volume;
 
-    async function findVideo() {
+    async function getVideoElement() {
         return new Promise((resolve) => {
             const observer = new MutationObserver((mutationsList, observer) => {
                 for (let mutation of mutationsList) {
@@ -37,7 +38,11 @@
         });
     }
 
-    async function findSliders() {
+    function getStoredVolume() {
+        return localStorage.getItem('fine_scroll-yt_music_volume');
+    }
+
+    async function getSliderElements() {
         return new Promise((resolve) => {
             const observer = new MutationObserver((mutationsList, observer) => {
                 for (let mutation of mutationsList) {
@@ -57,7 +62,7 @@
         });
     }
 
-    async function findSliderSetters() {
+    async function getSliderSetterElements() {
         return new Promise((resolve) => {
             const observer = new MutationObserver((mutationsList, observer) => {
                 for (let mutation of mutationsList) {
@@ -83,27 +88,54 @@
         return newElement;
     }
 
-    function setWheelBehaviour(videoElement, sliders, sliderSetters) {
-        const knobs = sliderSetters.filter(sliderSetter => sliderSetter.classList.contains('slider-knob'));
-        const sliderBars = sliderSetters.filter(sliderSetter => sliderSetter.id === 'sliderBar');
+    function setWheelBehaviour(videoElement, sliders, sliderSetters, preventer) {
         sliders.forEach(slider => {
             slider.addEventListener('wheel', (e) => {
+                videoElement.removeEventListener('volumechange', preventer);
                 if (e.deltaY > 0) {
                     videoElement.volume = Math.max(videoElement.volume - 0.01 * SLIDE_MULTIPLIER, 0);
                 } else {
                     videoElement.volume = Math.min(videoElement.volume + 0.01 * SLIDE_MULTIPLIER, 1);
                 }
-                sliderBars.forEach(sliderBar => sliderBar.value = videoElement.volume * 100);
-                knobs.forEach(knob => knob.style.left = `${videoElement.volume * 100}%`);
+                setVolumeVisually(videoElement, sliderSetters);
+                volume = videoElement.volume;
+                localStorage.setItem('fine_scroll-yt_music_volume', volume);
+                videoElement.addEventListener('volumechange', preventer);
             });
         });
     }
 
+    function setVolumeVisually(videoElement, sliderSetters) {
+        const knobs = sliderSetters.filter(sliderSetter => sliderSetter.classList.contains('slider-knob'));
+        const sliderBars = sliderSetters.filter(sliderSetter => sliderSetter.id === 'sliderBar');
+        sliderBars.forEach(sliderBar => sliderBar.value = videoElement.volume * 100);
+        knobs.forEach(knob => knob.style.left = `${videoElement.volume * 100}%`);
+
+    }
+
+    function preventInternalVolumeChanges(videoElement) {
+        let volChangeEvent = videoElement.addEventListener('volumechange', () => {
+            if (videoElement.volume !== volume) {
+                videoElement.volume = volume;
+            }
+        });
+        videoElement.addEventListener('play', () => {
+            videoElement.removeEventListener('volumechange', volChangeEvent)
+            videoElement.volume = volume
+            videoElement.addEventListener('volumechange', volChangeEvent)
+        })
+        return volChangeEvent
+    }
+
     async function run() {
-        const videoElement = await findVideo();
-        const sliders = await findSliders();
-        const sliderSetters = await findSliderSetters();
-        setWheelBehaviour(videoElement, sliders, sliderSetters);
+        const videoElement = await getVideoElement();
+        videoElement.volume = getStoredVolume() || 0.5;
+        volume = videoElement.volume;
+        const sliders = await getSliderElements();
+        const sliderSetters = await getSliderSetterElements();
+        setVolumeVisually(videoElement, sliderSetters);
+        let preventer = preventInternalVolumeChanges(videoElement);
+        setWheelBehaviour(videoElement, sliders, sliderSetters, preventer);
     }
 
     run();
