08abc7caf9a95319a857c9f6a2e1369804bcdf23
bl4cktrum <yavuzhancaskurlu@gmail.com>
2024-06-18 16:03:16 +0300

    exclude yt

--- fine_scroll-yt_music.js
+++ fine_scroll-yt_music.js
@@ -4,7 +4,7 @@
 // @version      2024-06-18
 // @description  try to take over the world!
 // @author       bl4cktrum
-// @match        *://*.youtube.com/watch?v=*
+// @match        *://music.youtube.com/watch?v=*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
